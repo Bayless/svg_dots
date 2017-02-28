461d2a8d02039aa57f39fe06d9cf33740a12699f
Bayless <bsmithsalzberg@stuy.edu>
2017-02-28 15:21:06 -0500

    did the thing

--- slate.js
+++ slate.js
@@ -7,25 +7,44 @@ function erase(event) {
     ctx.closePath();
     ctx.beginPath();
 }
+var prevX = -1;
+var prevY = -1;
 
 function circle(event) {
     var x = event.offsetX;     // Get the horizontal coordinate
     var y = event.offsetY;     // Get the vertical coordinate
-    
-    /*ctx.lineTo(x,y);
-    ctx.moveTo(x+10,y);
-    ctx.arc(x,y,10,0,2*Math.PI);
-    ctx.moveTo(x,y);
-    ctx.fill();
-    ctx.stroke();*/
-    <line x1="0" y1="0" x2="200" y2="200" style="stroke:rgb(255,0,0);stroke-width:2" />
-	var c = document.createElementNS("http://www.w3.org/2000/svg","circle");
-    var rando = Math.random() * 800;
-    c.setAttribute("cx",rando);
-    c.setAttribute("cy",rando);
-    c.setAttribute("r","10");
-    c.setAttribute("fill","#00f0fa");
-    esvg.appendChild(c);
+    //<line x1="0" y1="0" x2="200" y2="200" style="stroke:rgb(255,0,0);stroke-width:2" />
+    var c = document.createElementNS("http://www.w3.org/2000/svg","circle");
+    var d = document.createElementNS("http://www.w3.org/2000/svg","line");
+    if (prevX < 0){
+	c.setAttribute("cx",x);
+	c.setAttribute("cy",y);
+	c.setAttribute("r","10");
+	c.setAttribute("fill","#00f0fa");
+	prevX = x;
+	prevY = y;
+	esvg.appendChild(c);
+    }
+    else {
+	c.setAttribute("cx",x);
+	c.setAttribute("cy",y);
+	c.setAttribute("r","10");
+	c.setAttribute("fill","#00f0fa");
+	d.setAttribute("x1",prevX);
+	d.setAttribute("y1",prevY);
+	d.setAttribute("x2",x);
+	d.setAttribute("y2",y);
+	d.setAttribute("stroke","black");
+	console.log(prevX);
+	console.log(x);
+	console.log(prevY);
+	console.log(y);
+	prevX = x;
+	prevY = y;
+	esvg.appendChild(c);
+	esvg.appendChild(d);
+    }
+
 }
 
 clear.addEventListener("click", erase);

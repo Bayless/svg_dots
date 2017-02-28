1a5a416ee6be8d0d91f33f1b8c716425bb676cca
Bayless <bsmithsalzberg@stuy.edu>
2017-02-28 12:40:28 -0500

    random stuff

--- slate.js
+++ slate.js
@@ -19,9 +19,10 @@ function circle(event) {
     ctx.fill();
     ctx.stroke();*/
     <line x1="0" y1="0" x2="200" y2="200" style="stroke:rgb(255,0,0);stroke-width:2" />
-    var c = document.createElementNS("http://www.w3.org/2000/svg","circle");
-    c.setAttribute("cx","200");
-    c.setAttribute("cy","200");
+	var c = document.createElementNS("http://www.w3.org/2000/svg","circle");
+    var rando = Math.random() * 800;
+    c.setAttribute("cx",rando);
+    c.setAttribute("cy",rando);
     c.setAttribute("r","10");
     c.setAttribute("fill","#00f0fa");
     esvg.appendChild(c);
